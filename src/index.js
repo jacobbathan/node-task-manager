84b61b446dc4f299c83948af1312fcfb4297f3fb
jacobbathan <jacobbathan@gmail.com>
2019-06-14 11:25:04 -0700

    added some dope stuff its getting better

--- src/index.js
+++ src/index.js
@@ -12,16 +12,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server up on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-const main = async () => {
-  //  const task = await Task.findById('5d03d9c67f119e710463a368');
-  //  await task.populate('owner').execPopulate();
-  //  console.log(task.owner);
-  const user = await User.findById('5d03d837ff848470a0d05db0');
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks);
-};
-
-main();
