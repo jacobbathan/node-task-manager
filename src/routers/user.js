f9dd4154dabd596fcab8af41f72ce307954d2059
jacobbathan <jacobbathan@gmail.com>
2019-06-17 10:24:52 -0700

    added sharp for avatar image formating

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const multer = require('multer');
+const sharp = require('sharp');
 const auth = require('../middleware/auth');
 const User = require('../models/user');
 
@@ -71,7 +72,12 @@ router.post(
   auth,
   upload.single('avatar'),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -99,7 +105,7 @@ router.get('/users/:id/avatar', async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error('error');
     }
-    res.set('Content-Type', 'image/jpg');
+    res.set('Content-Type', 'image/png');
     res.send(user.avatar);
   } catch (error) {
     res.status(404).send({ error: error.message });
